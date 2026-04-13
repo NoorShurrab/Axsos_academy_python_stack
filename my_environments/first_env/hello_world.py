ec1f47dfd49ed435ba7194439732c3a4c67b1492
Noor Shurrab <fdlnwr88@gmail.com>
2026-04-13 17:51:00 +0300

    add first python file

---
+++ my_environments/first_env/hello_world.py
@@ -0,0 +1,20 @@
+# Task 1
+print("Hello World")
+
+# Task 2 
+name = "Noor"
+print("Hello", name + "!")
+print("Hello " + name + "!")
+
+# Task 3
+number = 1
+print("Hello", number)
+#print("Hello " + number) # error because it can't concatenate a string and an integer
+print(f"Hello {number}")
+
+# Task 4
+food_one = "chocolate"
+food_two = "chips"
+print(f"I love to eat {food_one} and {food_two}!")
+print("I love to eat {} and {}".format(food_one, food_two))
+

12679fc2174a641d6cc2c3364be27bb5e0867dd8
Noor Shurrab <fdlnwr88@gmail.com>
2026-04-23 16:59:29 +0300

    add function intermediate_2 file

---
+++ _python/python_fundamentals/Function_Intermediate_II_A9/function_intermediate_2.py
@@ -0,0 +1,77 @@
+# Update Values in Dictionaries and Lists
+x =[[5,2,3],[10,8,9]]
+students = [
+    {"first_name": "Michael", "last_name": "Jordan"},
+    {"first_name": "John", "last_name": "Rosales"},
+]
+sports_directory = {
+    'basketball': ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer': ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [{'x': 10, 'y': 20}]
+x[1][0] = 15
+print(x)
+students[0]["last_name"] = "Bryant"
+print(students)
+
+sports_directory['soccer'][0] = "Andres"
+print(sports_directory)
+
+z[0]["y"] =30
+print(z)
+
+print("--------------------------------")
+
+# Iterate Through a List of Dictionaries
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+def iterateDictionary(some_list):
+    for i in range(len(some_list)):
+        current_dict = some_list[i]
+        output = ""
+        for key in current_dict:
+            value = current_dict[key]
+            if output == "":
+                output += f"{key} - {value}"
+            else:
+                output += f", {key} - {value}"
+        print(output)
+
+iterateDictionary(students)
+print("--------------------------------")
+
+# Get Values From a List of Dictionaries
+def iterateDictionary2(key_name, some_list):
+    for i in range(len(some_list)):
+        current_dict = some_list[i]
+        if key_name in current_dict:
+            print(current_dict[key_name])
+iterateDictionary2('first_name', students)
+print("--------------------------------")
+iterateDictionary2('last_name', students)
+
+print("--------------------------------")
+
+# Iterate Through a Dictionary with List Values
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(some_dict):
+    for key in some_dict:
+        current_list = some_dict[key]
+        count = 0
+        for item in current_list:
+            count += 1
+        print(f"{count} {key}")
+
+        for i in range(count):
+            print(current_list[i])
+
+        print("")
+
+printInfo(dojo)

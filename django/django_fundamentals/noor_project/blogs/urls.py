ae36047511449016d85b2168fcdc79790950e306
Noor Shurrab <fdlnwr88@gmail.com>
2026-06-06 14:46:25 +0300

    update urls file for blogs app

--- django/django_fundamentals/noor_project/blogs/urls.py
+++ django/django_fundamentals/noor_project/blogs/urls.py
@@ -4,12 +4,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.root),                          
-    path('blogs', views.index),                    
-    path('blogs/new', views.new),    
-    path('blogs/create', views.create),                
-    path('blogs/<int:number>', views.show),          
-    path('blogs/<int:number>/edit', views.edit),     
-    path('blogs/<int:number>/delete', views.destroy),
-    path('blogs/json', views.json_bonus),            
+    #path('', views.root),                          
+    path('', views.index),      # Displays a list of all blogs              
+    path('new/', views.new),    # Displays a form to create a new blog
+    path('create/', views.create),    # Creates a new blog            
+    path('<int:number>/', views.show),   # Displays details of a specific blog 
+    path('<int:number>/edit/', views.edit),   # Displays a form to edit an existing blog  
+    path('<int:number>/delete/', views.destroy), # Deletes a specific blog 
+    #path('blogs/json', views.json_bonus),            
 ]
